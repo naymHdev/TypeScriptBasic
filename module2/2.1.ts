3943ba5a5c598f5db3bbfd0ea98758fc61b0d93d
MacBook Pro <naym100m@gmail.com>
2024-11-01 18:36:13 +0600

    [learn] assertion in the typescript

---
+++ module2/2.1.ts
@@ -0,0 +1,16 @@
+{
+  //
+
+  // Type assertion
+
+  let assertion: any;
+
+  assertion = "I love eating piZZa";
+  assertion = 700;
+
+  assertion as string; //.length
+
+  assertion as number; //.toFixed()
+
+  //
+}
